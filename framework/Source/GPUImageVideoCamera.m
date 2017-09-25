8ca374655999fb5d4f8667ae1ea8089c9c1e86c6
TRI VO <vominhtri1049@gmail.com>
2017-09-25 10:44:16 +0700

    Fix audio when record

--- framework/Source/GPUImageVideoCamera.m
+++ framework/Source/GPUImageVideoCamera.m
@@ -228,6 +228,9 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
     
     [_captureSession commitConfiguration];
     
+    ///
+    addedAudioInputsDueToEncodingTarget |= [self addAudioInputsAndOutputs];
+    
 	return self;
 }
 
@@ -279,9 +282,6 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
     [audioOutput setSampleBufferDelegate:self queue:audioProcessingQueue];
     
     [_captureSession commitConfiguration];
-    
-//    ///
-//    addedAudioInputsDueToEncodingTarget |= [self addAudioInputsAndOutputs];
     
     return YES;
 }
@@ -912,10 +912,10 @@ void setColorConversion709( GLfloat conversionMatrix[9] )
 {
     if (newValue) {
         /* Add audio inputs and outputs, if necessary */
-        addedAudioInputsDueToEncodingTarget |= [self addAudioInputsAndOutputs];
+//        addedAudioInputsDueToEncodingTarget |= [self addAudioInputsAndOutputs];
     } else if (addedAudioInputsDueToEncodingTarget) {
         /* Remove audio inputs and outputs, if they were added by previously setting the audio encoding target */
-        [self removeAudioInputsAndOutputs];
+//        [self removeAudioInputsAndOutputs];
         addedAudioInputsDueToEncodingTarget = NO;
     }
     
